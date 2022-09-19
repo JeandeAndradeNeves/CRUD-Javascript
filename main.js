f2b7b0a597a60994cedb48d0bfcdea9419b44464
JeandeAndradeNeves <jeandeneves@outlook.com>
2022-09-19 16:26:39 -0300

    CRUD

---
+++ main.js
@@ -0,0 +1,27 @@
+const openModal = () => document.getElementById('modal')
+    .classList.add('active');
+
+const closeModal = () => document.getElementById('modal')
+    .classList.remove('active');
+
+
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? [];
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient));
+
+// CRUD - create read update delete
+
+
+const createClient = (client) => {
+    const dbClient = getLocalStorage()
+    dbClient.push (client)
+    setLocalStorage(dbClient)
+};
+
+
+// Eventos
+document.getElementById('cadastrarCliente')
+    .addEventListener('click', openModal);
+
+document.getElementById('modalClose')
+    .addEventListener('click', closeModal);
